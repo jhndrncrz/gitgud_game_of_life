7f5a6472587e502d81c3e1a1630926ec1c30ff96
John Adrian Cruz <jhndrncrz@gmail.com>
2024-03-01 09:59:56 +0800

    added TODO list

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ import random
 import os
 import time
 
+# 1. branch: fix bug
+#
+# 2. branch: implement colors feature
+#       commit: add ansi colors dictionary
+#       commit: add one color to all cells
+#       commit: add random color to every cell
+#
+# 3. branch: implement iteration counter
+#       commit: add iteration counter
+#       commit: center iteration counter
+#
+# 4. merge fix bug branch
+# 5. rebase colors branch
+# 6. merge interation counter branch
+#
+# 7. commit: allow user input for board size
+# 8. commit: add validation for inputs
+
 def create_grid(rows: int, columns: int) -> list[list[int]]:
     return [[random.choice([0, 2]) for _ in range(columns)] for _ in range(rows)]
 
